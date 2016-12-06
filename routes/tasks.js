a5a5efbde13948e032f6f6196f48d1da8061ab63
Janne Haapsaari <haaja@iki.fi>
2016-12-06 12:06:50 +0200

    Fetch example tasks from db

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,5 +1,17 @@
 var express = require('express');
 var router = express.Router();
+var mongojs = require('mongojs');
+var db = mongojs('mongodb://mytasklist:rommikola123@ds113958.mlab.com:13958/mytasklist');
+
+
+router.get('/tasks', function(req, res, next) {
+    db.tasks.find(function(err, tasks) {
+	if (err) {
+	    res.send(err);
+	}
+	res.json(tasks);
+    });
+});
 
 
 router.get('/tasks', function(req, res, next) {
