049971c28b8ad6ace7437b86d16ca24fa31fab52
Janne Haapsaari <haaja@iki.fi>
2016-12-06 16:55:56 +0200

    Hook functionality to API

--- routes/tasks.js
+++ routes/tasks.js
@@ -25,9 +25,26 @@ router.get('/task/:id', function(req, res, next) {
     });
 });
 
-router.get('/tasks', function(req, res, next) {
-    res.send('TASKS');
+
+// Save task
+router.post('/task', function(req, res, next) {
+    var task = req.body;
+    if (!task.title || !(task.isDone + '')) {
+	res.status(400);
+	res.json({
+	    "error": "Bad data!"
+	});
+    }
+    else {
+	db.tasks.save(task, function(err, task) {
+	    if (err) {
+		res.send(err);
+	    }
+	    res.json(task);
+	});
+    }
 });
+
 
 // Delete task
 router.delete('/task/:id', function(req, res, next) {
@@ -38,6 +55,36 @@ router.delete('/task/:id', function(req, res, next) {
 	}
 	res.json(task);
     });
+});
+
+
+// Update task
+router.put('/task/:id', function(req, res, next) {
+    var task = req.body;
+    var updatedTask = {};
+
+    if (task.isDone) {
+	updatedTask.isDone = task.isDone;
+    }
+
+    if (task.title) {
+	updatedTask.title = task.title;
+    }
+
+    if (!updatedTask) {
+	res.status(400);
+	res.json({
+	    "error": "Bad data"
+	});
+    } else {
+	db.tasks.update({_id: mongojs.ObjectId(req.params.id)},
+			updatedTask, {}, function(err, task) {
+	    if (err) {
+		res.send(err);
+	    }
+	    res.json(task);
+	});
+    }
 });
 
 
