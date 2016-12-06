0b7633edaad574f9670ca58700682e03c94fb283
Janne Haapsaari <haaja@iki.fi>
2016-12-06 12:10:26 +0200

    Implement fetching single task

--- routes/tasks.js
+++ routes/tasks.js
@@ -4,6 +4,7 @@ var mongojs = require('mongojs');
 var db = mongojs('mongodb://mytasklist:rommikola123@ds113958.mlab.com:13958/mytasklist');
 
 
+//Get All tasks
 router.get('/tasks', function(req, res, next) {
     db.tasks.find(function(err, tasks) {
 	if (err) {
@@ -13,6 +14,15 @@ router.get('/tasks', function(req, res, next) {
     });
 });
 
+// Get Single task
+router.get('/task/:id', function(req, res, next) {
+    db.tasks.findOne({_id: mongojs.ObjectId(req.params.id)}, function(err, task) {
+	if (err) {
+	    res.send(err);
+	}
+	res.json(task);
+    });
+});
 
 router.get('/tasks', function(req, res, next) {
     res.send('TASKS');
